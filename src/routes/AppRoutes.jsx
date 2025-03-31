b1944f80d1fd2f0cac6c243cc1caf681c109cc70
Raj Maisuriya <maisuriyaraj664@gmail.com>
2025-03-31 23:04:43 +0530

    Code Commit for Registration page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthLayout } from '../layouts';
 import Login from '../pages/auth/login';
-
+import Register from '../pages/auth/register';
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/auth" element={<AuthLayout />} >
                     <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
                 </Route>
             </Routes>
         </Router>
